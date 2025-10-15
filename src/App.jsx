c749c3f05950d606b48705d7643791da23b29a3f
Prajwal <prajwal@git.com>
2025-10-16 00:11:41 +0530

    TaskBar and UseEff added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import ProgressBar from './components/ProgressBar'
 import Taskbar from './components/Taskbar'
 import TaskList from './components/TaskList'
-import { useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
 
+  const addTask = (Task) => {
+    setTasks([...tasks, Task]);
+  }
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    console.log('Eff Running');
+  });
+
   return (
     <div className='max-w-3xl mt-16 border border-gray-100 shadow-md mx-auto p-7'>
       <h1 className='text-4xl font-bold mb-2 text-gray-800/90'>Planora</h1>
       <p className='font-semibold mb-6 text-gray-700/80'>Our Friendly TaskManager</p>
-      <Taskbar />
+      <Taskbar addTask={addTask} />
       <TaskList />
       <ProgressBar />
       {tasks.length > 0 ?
-      <button className='bg-red-500 text-white px-6 py-1 rounded text-lg hover:bg-red-700 hover:scale-105'>Clear All Tasks</button>  
-      : '' }
-      
+        <button className='bg-red-500 text-white px-6 py-1 rounded text-lg hover:bg-red-700 hover:scale-105'>Clear All Tasks</button>
+        : ''}
+
     </div>
   )
 }
--- src/components/Taskbar.jsx
+++ src/components/Taskbar.jsx
@@ -1,23 +1,57 @@
-import React from 'react'
+import { useState } from 'react'
 
-const Taskbar = () => {
-  
+const Taskbar = ({ addTask }) => {
+  const [Task, setTask] = useState({
+    text: '',
+    priority: 'low',
+    category: 'general',
+    completed: false,
+  })
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    addTask(Task);
+
+    setTask({
+      text: '',
+      priority: 'low',
+      category: 'general',
+      completed: false,
+    });
+  }
 
   return (
     <div className='w-full mb-6'>
-      <form className='w-full flex gap-3 flex-col md:flex-row'>
+      <form
+        className='w-full flex gap-3 flex-col md:flex-row'
+        onSubmit={handleForm}
+      >
         <div className='relative flex-1 flex items-center justify-center'>
-          <input type="text" name="tasks" placeholder='Enter task here...'
-            className='px-2 py-2 border w-full border-gray-200 rounded-lg focus:outline-none' />
+          <input type="text"
+            name="text"
+            value={Task.text}
+            placeholder='Enter task here...'
+            className='px-2 py-2 border w-full text-gray-600 border-gray-200 rounded-lg focus:outline-none'
+            onChange={(e) => setTask({ ...Task, [e.target.name]: e.target.value })}
+          />
           <button className='bg-gray-700 absolute right-0 inset-y-0 text-white py-2 px-4 rounded-lg hover:bg-gray-900 hover:scale-105'>Add Task</button>
         </div>
-        <div className='flex gap-1'>
-          <select name='priority' className='border border-gray-200 rounded-lg focus:outline-none pl-1 max-md:py-2'>
+        <div className='flex gap-2 font-medium text-gray-600/80'>
+          <select name='priority'
+            className='border border-gray-200 rounded-lg focus:outline-none pl-1 max-md:py-2'
+            value={Task.priority}
+            onChange={(e) => setTask({ ...Task, [e.target.name]: e.target.value })}
+          >
             <option value="low">Low</option>
             <option value="medium">Medium</option>
             <option value="high">High</option>
           </select>
-          <select name='category' className='border border-gray-200 rounded-lg focus:outline-none pl-1'>
+          <select name='category'
+            className='border border-gray-200 rounded-lg focus:outline-none pl-1'
+            value={Task.category}
+            onChange={(e) => setTask({ ...Task, [e.target.name]: e.target.value })}
+          >
             <option value="general">General</option>
             <option value="work">Work</option>
             <option value="personal">Personal</option>
